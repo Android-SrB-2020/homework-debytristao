26a55f8167324aacb72ac3125929d5d09e29900a
Debora Tristao <48920353+debytristao@users.noreply.github.com>
2020-01-08 19:18:39 -0400

    1 First Apps

---
+++ 1 First Apps/HelloWorld/app/src/main/java/work/nbcc/helloworld/MainActivity.kt
@@ -0,0 +1,14 @@
+package work.nbcc.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //var btn = findViewById(R.id.button) as Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
